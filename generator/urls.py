bf594a766641330dfc0a864fcdc0055afc7a626e
Rakhat Shalgynbay <rakhatshalgynbay@gmail.com>
2023-01-18 00:36:45 +0600

    First commit!

---
+++ generator/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('', home, name='home'),
+    path('password/', password, name='password'),
+    path('about/', about, name='about'),
+]
